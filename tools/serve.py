651e187f005c748fc0fbe4b2581556c3f9793cc7
xufeng-1 <252878339@qq.com>
2026-08-04 16:25:04 +0800

    ?? xu???? ????

---
+++ tools/serve.py
@@ -0,0 +1,7 @@
+﻿import http.server, socketserver, os, functools
+root = r"C:\Users\Thinkpad\Documents\Codex\2026-08-03\new-chat\work\xu-workbench\docs"
+os.chdir(root)
+handler = functools.partial(http.server.SimpleHTTPRequestHandler, directory=root)
+with socketserver.TCPServer(("127.0.0.1", 8899), handler) as httpd:
+    print("serving on 8899", flush=True)
+    httpd.serve_forever()
